7a32b5f92bea5c88f23461725080d7670b8f0729
marcelo.fortes <marcelo.fortes@nostrum.com.br>
2024-04-30 15:02:22 -0300

    favorite actions

--- lib/features/favorites/favorites_page.dart
+++ lib/features/favorites/favorites_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:very_good_coffee_app/features/favorites/favorite_image_gallery.dart';
+import 'package:very_good_coffee_app/features/shared/animated_options.dart';
+import 'package:very_good_coffee_app/features/shared/custom_colors.dart';
 import 'package:very_good_coffee_app/features/shared/images_provider.dart';
 
 class FavoritesPage extends StatefulWidget {
@@ -11,53 +13,110 @@ class FavoritesPage extends StatefulWidget {
 }
 
 class _FavoritesPageState extends State<FavoritesPage> {
-  List<String> selectedImages = [];
-  bool _selectionActive = false;
+  final List<String> _selectedImages = [];
 
   _onSelectImage(String url) {
-    if (selectedImages.contains(url)) {
-      selectedImages.remove(url);
+    if (_selectedImages.contains(url)) {
+      _selectedImages.remove(url);
     } else {
-      selectedImages.add(url);
+      _selectedImages.add(url);
     }
 
-    _selectionActive = selectedImages.isNotEmpty;
+    setState(() {});
+  }
+
+  _unlikeAllSelected() {
+    final provider = Provider.of<ImagesProvider>(context, listen: false);
+
+    for (var e in _selectedImages) {
+      provider.toggleLikedImage(e);
+    }
+
+    _selectedImages.clear();
+    setState(() {});
+  }
+
+  _cancelSelection() {
+    _selectedImages.clear();
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     final provider = context.watch<ImagesProvider>();
-    return Container(
-      color: Theme.of(context).scaffoldBackgroundColor,
-      padding: const EdgeInsets.all(16),
-      child: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Your favorite photos',
-              style: Theme.of(context).textTheme.titleLarge,
+    return Stack(
+      children: [
+        Container(
+          color: Theme.of(context).scaffoldBackgroundColor,
+          padding: const EdgeInsets.all(16),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Your favorite photos',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const Divider(),
+                Expanded(
+                  child: provider.likedImages.isEmpty
+                      ? const Center(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.favorite,
+                                size: 56,
+                                color: Colors.white,
+                              ),
+                              Divider(),
+                              Text("You don't have any liked photos yet.")
+                            ],
+                          ),
+                        )
+                      : GridView.count(
+                          crossAxisSpacing: 16,
+                          mainAxisSpacing: 16,
+                          crossAxisCount: 2,
+                          children: provider.likedImages.map((e) {
+                            return FavoriteImageGallery(
+                              url: e,
+                              isSelected: _selectedImages.contains(e),
+                              onSelect: (url) => _onSelectImage(url),
+                              selectionActive: _selectedImages.isNotEmpty,
+                            );
+                          }).toList(),
+                        ),
+                ),
+              ],
             ),
-            const Divider(),
-            Expanded(
-              child: GridView.count(
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                crossAxisCount: 2,
-                children: provider.likedImages.map((e) {
-                  return FavoriteImageGallery(
-                    url: e,
-                    isSelected: selectedImages.contains(e),
-                    onSelect: (url) => _onSelectImage(url),
-                    selectionActive: _selectionActive,
-                  );
-                }).toList(),
+          ),
+        ),
+        AnimatedOptions(
+          isVisible: _selectedImages.isNotEmpty,
+          children: [
+            TextButton.icon(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: CustomColors.secondaryColor,
               ),
+              onPressed: _unlikeAllSelected,
+              icon: const Icon(Icons.heart_broken_rounded),
+              label: const Text("Unlike Selected"),
+            ),
+            const Spacer(),
+            TextButton.icon(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: CustomColors.secondaryColor,
+              ),
+              onPressed: _cancelSelection,
+              icon: const Icon(Icons.close),
+              label: const Text("Cancel"),
             ),
           ],
-        ),
-      ),
+        )
+      ],
     );
   }
 }
---
+++ lib/features/shared/animated_options.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class AnimatedOptions extends StatefulWidget {
+  const AnimatedOptions({
+    super.key,
+    required this.isVisible,
+    required this.children,
+  });
+  final bool isVisible;
+  final List<Widget> children;
+
+  @override
+  AnimatedOptionsState createState() => AnimatedOptionsState();
+}
+
+class AnimatedOptionsState extends State<AnimatedOptions>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+  bool _isVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    );
+
+    // Inicializa a animação baseada no estado inicial de isVisible
+    if (widget.isVisible) {
+      _animationController.forward();
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant AnimatedOptions oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // Atualiza a animação quando o valor de isVisible muda
+    if (widget.isVisible != oldWidget.isVisible) {
+      if (widget.isVisible) {
+        _animationController.forward();
+      } else {
+        _animationController.reverse();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+      if (_isVisible) {
+        _animationController.forward();
+      } else {
+        _animationController.reverse();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return Align(
+          alignment: Alignment.bottomCenter,
+          child: Transform.translate(
+            offset: Offset(0.0, (1 - _animation.value) * 100),
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.grey[800],
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: widget.children,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
